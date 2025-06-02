9fa8e349baad6f68a3a5ff6bd45bfef9d45426e7
Antono James <49890403+asjames18@users.noreply.github.com>
2025-06-02 14:40:50 -0400

    update

---
+++ sdk-samples/card-preview/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CacheCard from '../../src/cards/CacheCard';
+
+function App() {
+  // You may need to mock or provide any required props here
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>CacheCard Preview</h1>
+      <CacheCard />
+    </div>
+  );
+}
+
+export default App;
